cf289e031f7774bd9796cdc0e0e847ad8a2daef9
Ahmedismail <144509690+AHMEDSH72@users.noreply.github.com>
2023-10-21 16:57:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const input = document.querySelector('input[type=checkbox]');
+
+input.addEventListener('click', () => {
+    const monthly = document.querySelector('.monthly');
+    const annually = document.querySelector('.annually');
+
+    monthlyHasHidden = monthly.classList.toggle("hidden");
+    annuallyHasHidden = annually.classList.toggle("hidden");
+
+    // note for my understanding. It was somewhat confusing
+    if (monthlyHasHidden) {
+        console.log("if monthly does not have the class name 'hidden', add it and returns ", monthlyHasHidden);
+
+    } else {
+        console.log("if monthly has the class name 'hidden', remove it and returns ", monthlyHasHidden);
+
+    }
+
+
+
+});
